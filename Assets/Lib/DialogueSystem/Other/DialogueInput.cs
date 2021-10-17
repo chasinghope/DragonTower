c33ad2e2d22f74fb27d94edb965746d708d9be49
Bob <bobjiang1045@163.com>
2021-10-17 22:52:32 +0800

    10.17会议记录

--- Assets/Lib/DialogueSystem/Other/DialogueInput.cs
+++ Assets/Lib/DialogueSystem/Other/DialogueInput.cs
@@ -5,27 +5,41 @@ using UnityEngine.InputSystem;
 
 public class DialogueInput : MonoBehaviour
 {
-    public InputAction QKey;
-    public InputAction SpaceKey;
+    //public InputAction QKey;
+    //public InputAction SpaceKey;
     public DialogueCanvas diaCanvas;
 
-    private void Awake()
+    private void Update()
     {
-        QKey.performed += ctx => { diaCanvas.DoAction_Dialogue(); };
-        SpaceKey.performed += ctx => { diaCanvas.DoAction_ContinueTalk(); };
+        if (Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            diaCanvas.DoAction_Dialogue();
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            diaCanvas.DoAction_ContinueTalk();
+        }
     }
 
-    private void OnEnable()
-    {
-        QKey.Enable();
-        SpaceKey.Enable();
-    }
+
+    //private void Awake()
+    //{
+    //    QKey.performed += ctx => { Debug.Log("sssss"); diaCanvas.DoAction_Dialogue(); };
+    //    SpaceKey.performed += ctx => { Debug.Log("aaaaa");   diaCanvas.DoAction_ContinueTalk(); };
+    //}
+
+    //private void OnEnable()
+    //{
+    //    QKey.Enable();
+    //    SpaceKey.Enable();
+    //}
 
 
-    private void OnDisable()
-    {
-        QKey.Disable();
-        SpaceKey.Disable();
-    }
+    //private void OnDisable()
+    //{
+    //    QKey.Disable();
+    //    SpaceKey.Disable();
+    //}
 
 }
